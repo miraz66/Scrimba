35793adc8854b5dd79ca9a73ce81150a59d49405
miraz66 <mirajulislam0509@gmail.com>
2023-10-19 23:14:07 +0600

    array

--- Test.py
+++ Test.py
@@ -5,14 +5,20 @@ msg1 = msg[18] + " " + msg[:8] + msg[8:10] + msg[10:17]
 print(msg1)
 print(msg.replace("Strings", "Release"))
 
-# input creacor`python`
+# -----input creacor`python-----`
+
 # name = input("What is your name? : ")
 # age = input("What is your age? : ")
 
 # print("Hello " + name + "! You are " + age + " years old.")
 
-name = input("Enter your name: ")
-distance_km = input("Enter your distance_km: ")
-distance_mi = float(distance_km) / 1.609
+# name = input("Enter your name: ")
+# distance_km = input("Enter your distance_km: ")
+# distance_mi = int(distance_km) / 1.609
 
-print(f"Hi {name.title()}! {distance_km} Km is equivalent to {distance_mi} miles.")
+# print(f"Hi {name.title()}! {distance_km} Km is equivalent to {round(distance_mi,1)} miles.")
+
+# -----Array of distances -----
+friends = ["Inzam", "Al-amin", "Al-amin-E",
+           "Monju", "Monju-E", "Tuhin", "Tuhin-E",]
+cars = [911, 471, 158, 177, 28, 228, 255,]
