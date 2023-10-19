025eea61e301523c7acf8c36faca8ae7d0155d14
miraz66 <mirajulislam0509@gmail.com>
2023-10-19 21:41:30 +0600

    Test

---
+++ Test.py
@@ -0,0 +1,18 @@
+msg = 'welcome to Python 101: Strings'
+msg1 = msg[18] + " " + msg[:8] + msg[8:10] + msg[10:17]
+
+
+print(msg1)
+print(msg.replace("Strings", "Release"))
+
+# input creacor`python`
+# name = input("What is your name? : ")
+# age = input("What is your age? : ")
+
+# print("Hello " + name + "! You are " + age + " years old.")
+
+num1 = input("Enter your Digits: ")
+num2 = input("Enter your second digit: ")
+
+answer = float(num1) + float(num2)
+print(answer)
