a29d1cc968c8235a05e6e81da0ea8f450299cade
miraz66 <mirajulislam0509@gmail.com>
2023-10-26 01:17:47 +0600

    Function

--- Fuction.py
+++ Fuction.py
@@ -18,12 +18,37 @@ print('o in John is ', 'o' not in 'John')  # non membership
 print('John is John ', 'John' is 'John')  # identity
 print('John is not John is ', 'John' is not 'John')  # negative identity
 '''
-
+'''
 is_raining = False
 is_cold = False
-
 print("Good Morning")
-if is_raining or is_cold:
-    print("Bring Umbrella or jacket or both")
+if is_raining and is_cold:
+    print("Bring Umbrella and jacket")
+elif is_raining and not (is_cold):
+    print("Bring Umbrella")
+elif not (is_raining) and is_cold:
+    print("Bring Jacket")
 else:
-    print("Umbrella is optional")
+    print("Shirt is fine!")
+'''
+
+
+'''num1 = float(input('Enter first number: '))
+mode = input('Enter math operation(+,-,*,/): ')
+num2 = float(input('Enter second number: '))
+
+if mode == '+':
+    print(f'Answer is: {num1 + num2}')
+elif mode == '-':
+    print(f'Answer is: {num1 - num2}')
+elif mode == '*':
+    print(f'Answer is: {num1 * num2}')
+elif mode == '/':
+    print(f'Answer is: {num1 / num2}')
+else:
+    print('Input error!')'''
+
+mode = input("Enter your Calculation: ")
+
+
+print(mode)
