56e08691f16dc61cd06a08c94be3f3a0cb646440
miraz66 <mirajulislam0509@gmail.com>
2023-10-26 00:21:28 +0600

    Function

--- Fuction.py
+++ Fuction.py
@@ -3,3 +3,27 @@ def greetings(name, age):
 
 
 greetings("Miraz", 27)
+
+'''
+a = 7
+b = 3
+print('a == b is', a == b)
+print('a != b is', a != b)
+print('a > b is', a > b)
+print('a < b is', a < b)
+print('a >= b is', a >= b)
+print('a <= b is', a <= b)
+print('o in John is ', 'o' in 'John')  # membership
+print('o in John is ', 'o' not in 'John')  # non membership
+print('John is John ', 'John' is 'John')  # identity
+print('John is not John is ', 'John' is not 'John')  # negative identity
+'''
+
+is_raining = False
+is_cold = False
+
+print("Good Morning")
+if is_raining or is_cold:
+    print("Bring Umbrella or jacket or both")
+else:
+    print("Umbrella is optional")
