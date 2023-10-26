028f404aa57e2d704c44825d6060c482a84be2a0
miraz66 <mirajulislam0509@gmail.com>
2023-10-27 01:01:11 +0600

    condition

--- Fuction.py
+++ Fuction.py
@@ -57,3 +57,5 @@ array = "Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion
 array = "Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion:"
 array = "Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion:"
 array = "Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion:"
+array = "Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion:"
+array = "Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion:"
