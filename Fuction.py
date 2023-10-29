d4a260b4a825454784db1d2a818d6679679e36a7
miraz66 <mirajulislam0509@gmail.com>
2023-10-29 23:21:32 +0600

    while loop

--- Fuction.py
+++ Fuction.py
@@ -60,39 +60,26 @@ while i < 5:
     print(f'{i}.' + '*' * i + 'while loop are awesome' + '*' * i)
 
 '''
-'''
-num = 12
-guess = 0
-guess_limit = 3
-guess_number = 0
-
-while guess_number < guess_limit:
-    guess = int(
-        input(f'Guess # {guess_number + 1} a number 1-20: last guess:{guess} '))
-    if guess == num:
-        print(f'You Win! You Guessed it: {guess}')
-        break
-    else:
-        print(f'No, not {guess}!')
-        guess_number += 1
-if guess != num:
-    print(f'Sorry you lose! It was {num}')
-'''
 
 num = 12
 guess = 0
 guess_limit = 3
 guess_number = 1
+guess = int(
+    input(f'Guess # {guess_number } a number 1-20: last guess:{guess} '))
+guess_number += 1
 
 while guess_number <= guess_limit:
-    guess = int(
-        input(f'Guess # {guess_number } a number 1-20: last guess:{guess} '))
+
+    if guess != num:
+        guess_number += 1
+        if guess > num:
+            guess = int(input(f'{guess} Too high - Guess again 1-100: '))
+        else:
+            guess = int(input(f'{guess} too low - Guess again 1-100: '))
     if guess == num:
         print(f'You Win! You Guessed it: {guess}')
         break
-    else:
-        print(f'No, not {guess}!')
-        guess_number += 1
+
 if guess != num:
     print(f'Sorry you lose! It was {num}')
-4
