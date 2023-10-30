edeca90a5c18a0e48224d93d373b6b6dbbf061cb
miraz66 <mirajulislam0509@gmail.com>
2023-10-30 22:31:02 +0600

    for loop

--- Fuction.py
+++ Fuction.py
@@ -130,3 +130,5 @@ for num, name in enumerate(friends, 1):
     print(f"{num}.{name.title()}! {msg}")
     print(f"{num}.{name.title()}! {msg}")
     print(f"{num}.{name.title()}! {msg}")
+    print(f"{num}.{name.title()}! {msg}")
+    print(f"{num}.{name.title()}! {msg}")
