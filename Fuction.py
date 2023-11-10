4cba4d48da37aded1539a0ca96d3916572bc0e8b
miraz66 <mirajulislam0509@gmail.com>
2023-11-10 23:42:03 +0600

    Sorted

--- Fuction.py
+++ Fuction.py
@@ -146,3 +146,10 @@ print(sorted(my_llist, key=lambda item: item[3]))'''
 print(movie.get('', "Not Fount!",))
 for key, value in movie.items():
     print(key, value)'''
+
+python = {'John': 35, 'Eric': 36, 'Michael': 35,
+          'Terry': 38, 'Graham': 37, 'TerryG': 34}
+holy_grail = {'Arthur': 40, 'Galahad': 35,
+              'Lancelot': 39, 'Knight of NI': 40, 'Zoot': 17}
+life_of_brian = {'Brian': 33, 'Reg': 35,
+                 'Stan/Loretta': 32, 'Biccus Diccus': 45}
